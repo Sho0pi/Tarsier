b075b93341224b76d9188b5c59b1e2b847b198fe
Sho0pi <itayblokh@gmail.com>
2020-02-17 12:04:32 +0200

    generating the generate random time function

--- main.go
+++ main.go
@@ -2,7 +2,8 @@ package main
 
 
 import (
-
+	"time"
+	"math/rand"
 )
 
 const (
@@ -15,4 +16,26 @@ var (
 
 func main()	{
 
+}
+
+/**
+ * @brief      For a given maxTimeout will return a random time.Duration
+ *
+ * @param      maxTimeout  The maximum timeout
+ *
+ * @return     a time within that range (0,maxTimeout]
+ */
+func generateRandomTimeout(maxTimeout int) time.Duration {
+	rand.Seed(generateRandomSeed())
+	n := rand.Intn(maxTimeout)
+	return time.Duration(maxTimeout - n)
+}
+
+/**
+ * @brief      This function will generate random seed from the ephoktime
+ *
+ * @return     the current UNIX time
+ */
+func generateRandomSeed() int64{
+	return time.Now().Unix()
 }
