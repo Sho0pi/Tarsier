0f4882bc5b64ccbec1a4a9415c27831a424593a0
Sho0pi <itayblokh@gmail.com>
2020-02-17 11:51:28 +0200

    creating the execute method, that will execute the xeyes for timeout time

--- main.go
+++ main.go
@@ -2,7 +2,8 @@ package main
 
 
 import (
-
+	"os/exec"
+	"time"
 )
 
 const (
@@ -14,5 +15,38 @@ var (
 )
 
 func main()	{
+
+}
+
+/**
+ * @brief      This function will execute the xeyes and will stop it after random time
+ *
+ * @param 	   timeout the amount of time to execute
+ *
+ * @return     an error if accure
+ */
+func execute(timeout time.Duration) error{
+
+	cmd := exec.Command(XEYES_PATH)
+	if err := cmd.Start(); err !=nil {
+		return err
+	}
+
+	done := make(chan error, 1)
+	go func(){
+		done <- cmd.Wait()
+	}()
+
+	select {
+	case <-time.After(timeout * time.Second):
+		if err := cmd.Process.Kill(); err != nil {
+			return err
+		}
+	case err := <-done :
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 
 }
